0c3236c682b67c95ff8cd077c7bbc42498d41406
zhuwenlong <zhuwenlong@autohome.com.cn>
2022-01-26 16:56:54 +0800

    feat:pm2后台提供服务

---
+++ ecosystem.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  apps: [
+    {
+      name: 'limit worker',
+      script: './server.js',
+      args: 'limit',
+    },
+    // {
+    //   name: 'rotate worker',
+    //   script: './server.js',
+    //   args: 'rotate',
+    // },
+  ],
+};
